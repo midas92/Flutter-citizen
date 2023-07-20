809c643ef25c93b9976e4c69e3b18aa463036c5a
midas92 <dounanpar@gmail.com>
2023-07-19 20:20:28 -0400

    add shadow to  homepage components

--- lib/views/home/widgets/update_data_section.dart
+++ lib/views/home/widgets/update_data_section.dart
@@ -17,70 +17,82 @@ class UpdateDataSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       flex: 21,
-      child: Indexer(
-        children: [
-          Indexed(
-            index: 0,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.5),
-                borderRadius: BorderRadius.circular(12),
-                border: Border.all(width: 1.0, color: Colors.grey),
-              ),
-              padding: const EdgeInsets.all(16),
-              child: Stack(
-                children: [
-                  Positioned(
-                      top: 0,
-                      right: 0,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        textDirection: TextDirection.ltr,
-                        children: [
-                          Text(
-                            Strings.homeUpdateComment,
-                            textAlign: TextAlign.right,
-                            style: CustomTextStyle.LabelText(
-                                ThemeColors.secondary, 16, FontWeight.w400),
-                          ),
-                          const SizedBox(height: 5),
-                          Text(Strings.homeUpdateDate,
-                              textAlign: TextAlign.right,
-                              style: CustomTextStyle.SpanText())
-                        ],
-                      )),
-                  Positioned(
-                      bottom: 10,
-                      left: 0,
-                      child: Text(
-                        Strings.homeUpdateQuestion,
-                        style: CustomTextStyle.LabelText(),
-                      )),
-                  Positioned(
-                      bottom: 0,
-                      right: 0,
-                      child: RoundedButton(
-                          onPressed: () {},
-                          text: Strings.homeUpdateButton,
-                          backgroundColor:
-                              Theme.of(context).colorScheme.secondary,
-                          foregroundColor: Colors.white)),
-                ],
-              ),
-            ),
+      child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: Colors.transparent,
+            boxShadow: const [
+              BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 1.0, // soften the shadow
+                  spreadRadius: 1.0,
+                  blurStyle: BlurStyle.outer),
+            ],
           ),
-          Indexed(
-              index: 1,
-              child: Positioned(
-                  top: -10,
-                  left: 16,
-                  child: SvgPicture.asset(
-                    "assets/icons/speaker.svg",
-                    width: 60,
-                  ))),
-        ],
-      ),
+          child: Indexer(
+            children: [
+              Indexed(
+                index: 0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.5),
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(width: 1.0, color: Colors.grey),
+                  ),
+                  padding: const EdgeInsets.all(16),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                          top: 0,
+                          right: 0,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            textDirection: TextDirection.ltr,
+                            children: [
+                              Text(
+                                Strings.homeUpdateComment,
+                                textAlign: TextAlign.right,
+                                style: CustomTextStyle.LabelText(
+                                    ThemeColors.secondary, 16, FontWeight.w400),
+                              ),
+                              const SizedBox(height: 5),
+                              Text(Strings.homeUpdateDate,
+                                  textAlign: TextAlign.right,
+                                  style: CustomTextStyle.SpanText())
+                            ],
+                          )),
+                      Positioned(
+                          bottom: 10,
+                          left: 0,
+                          child: Text(
+                            Strings.homeUpdateQuestion,
+                            style: CustomTextStyle.LabelText(),
+                          )),
+                      Positioned(
+                          bottom: 0,
+                          right: 0,
+                          child: RoundedButton(
+                              onPressed: () {},
+                              text: Strings.homeUpdateButton,
+                              backgroundColor:
+                                  Theme.of(context).colorScheme.secondary,
+                              foregroundColor: Colors.white)),
+                    ],
+                  ),
+                ),
+              ),
+              Indexed(
+                  index: 1,
+                  child: Positioned(
+                      top: -10,
+                      left: 16,
+                      child: SvgPicture.asset(
+                        "assets/icons/speaker.svg",
+                        width: 60,
+                      ))),
+            ],
+          )),
     );
   }
 }
